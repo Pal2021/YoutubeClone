444955d516831d827abede2629f7cb4114a14a19
prashant <prashantpal200013@gmail.com>
2024-02-19 10:45:24 +0530

    Add video and User Model

--- src/index.js
+++ src/index.js
@@ -1,34 +1,21 @@
 import dotenv from "dotenv";
+import express from "express";
 import connectDB from "./db/index.js";
+
 dotenv.config({
   path: "./env",
 });
+
+const app = express();
+
 connectDB()
   .then(() => {
-    app.listen(process.env.PORT || 8000, () => {
-      console.log(` Server is running at PORT : ${process.env.PORT}`);
-    });
-    app.on("error", () => {
-      console.log("ERROR ", error);
-      throw error;
+    const port = process.env.PORT || 8000;
+    app.listen(port, () => {
+      console.log(`Server is running at PORT: ${port}`);
     });
   })
   .catch((error) => {
-    console.log("MONGO db connection failed !!!", error);
+    console.error("MongoDB connection failed:", error);
+    process.exit(1);
   });
-
-// (async () => {
-//   try {
-//     mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
-//     application.on("error", () => {
-//       console.log("ERROR ", error);
-//       throw error;
-//     });
-//     app.listen(process.env.PORT, () => {
-//       console.log(`App is listen on port ${process.env.PORT}`);
-//     });
-//   } catch (error) {
-//     console.log("ERROR: ", error);
-//     throw err;
-//   }
-// })();
